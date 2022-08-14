0db1d0f33c98d13977905e814a9bf12aee881278
Edwin Campos <ecamposc@uni.pe>
2022-08-14 01:36:03 -0500

    Ejs as view engine.

--- app.js
+++ app.js
@@ -22,19 +22,20 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
 /*Print Text from Read result*/
 function printRecText(readResults) {
-        console.log('Recognized text:');
+        let string_rec='';
         for (const page in readResults) {
           if (readResults.length > 1) {
-            console.log(`==== Page: ${page}`);
+            string_rec += `==== Page: ${page}`;
           }
           const result = readResults[page];
           if (result.lines.length) {
             for (const line of result.lines) {
-              console.log(line.words.map(w => w.text).join(' '));
+              string_rec += line.words.map(w => w.text).join(' ');
             }
           }
-          else { console.log('No recognized text.'); }
+          else { string_rec += 'No recognized text.'; }
         }
+        return string_rec;
       }
 
 // Perform read and await the result from local file
@@ -45,7 +46,7 @@ async function readTextFromLocal(client, path, res) {
       return result;
     })
     .catch((err)=>{
-      res.sendFile(`${ __dirname }/failed.html`);
+      res.render('failed');
       post_successful = false;
     });
   if (!post_successful){ return 0; }
@@ -59,12 +60,13 @@ async function readTextFromLocal(client, path, res) {
     const read_result = await client.getReadResult(operation);
     console.log("STATUS", read_result.status);
     if (read_result.status == 'succeeded'){
-      console.log(printRecText(read_result.analyzeResult.readResults));
-      res.sendFile(`${ __dirname }/results.html`);
+      const textFound = printRecText(read_result.analyzeResult.readResults);
+      console.log(textFound);
+      res.render('results', {textFound: textFound, imgPath: path});
       break;
     }
     else if(read_result.status == 'failed'){
-      res.sendFile(`${ __dirname }/failed.html`);
+      res.render('failed');
       break;
     }
     await delay(1000);
@@ -74,9 +76,10 @@ async function readTextFromLocal(client, path, res) {
 app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/',(req, res)=>{
-  res.sendFile(`${ __dirname }/index.html`);
+  res.render('index');
 })
 
 app.post('/', upload.single("im_file"), (req,res)=>{
@@ -85,16 +88,21 @@ app.post('/', upload.single("im_file"), (req,res)=>{
   console.log(req.body);
   console.log(req.file);
 
+  fs.copyFile(req.file.path, `public/${ req.file.path }`,(err)=>{
+    if (err){throw err;}
+    console.log(`succesful copy to ${ req.file.path }`)
+  })
+
   readTextFromLocal(cvClient, req.file.path, res);
 
 })
 
 app.get('/results', (req,res)=>{
-  res.sendFile(`${ __dirname }/results.html`);
+  res.render('results', {textFound: 'No file selected', imgPath: 'no image',});
 })
 
 app.get('/contact', (req,res)=>{
-  res.sendFile(`${ __dirname }/contact.html`);
+  res.render('contact');
 })
 
 app.listen(process.env.PORT || 3000, ()=>{
